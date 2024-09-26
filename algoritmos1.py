99743bbe305a298a78a4772d2241a268d5fead3a
LucCas <158449085+LucaGryCas@users.noreply.github.com>
2024-09-27 00:43:24 +0200

    docstrings

--- algoritmos1.py
+++ algoritmos1.py
@@ -1,5 +1,29 @@
 
+
 def searchpairswithlists(lista,target):
+    '''
+    Usa listas para encontrar los pares de elementos de 
+    una lista entera ordenada que sumen target
+
+    Parameters
+    ----------
+    lista : TYPE
+            list
+        DESCRIPTION.
+        lista ordenada de números enteros
+    target : TYPE
+            int
+        DESCRIPTION.
+        Número objetivo 
+
+    Returns
+    -------
+    pairs : TYPE
+            list
+        DESCRIPTION.
+        pares pertenecientes a la lista que suman dos
+
+    '''
     pairs= []
     for i in range(len(lista)):
         for j in range(i,len(lista)):
@@ -9,8 +33,31 @@ def searchpairswithlists(lista,target):
     return pairs
 
 
-
 def searchpairswithsets(lista,target):
+    '''
+    Usa sets para encontrar los pares de elementos de 
+    una lista entera ordenada que sumen target
+
+
+    Parameters
+    ----------
+    lista : TYPE
+            list
+        DESCRIPTION.
+        lista de números enteros ordenada
+    target : TYPE
+            int
+        DESCRIPTION.
+        Número objetivo
+
+    Returns
+    -------
+    pairs : TYPE
+            list
+        DESCRIPTION.
+        lista de pares de la lista que suman el objetivo
+
+    '''
     pairs= set()
     seen= set()
     for i in range(len(lista)):
@@ -19,4 +66,3 @@ def searchpairswithsets(lista,target):
         if c in seen:
             pairs.add((min(c,lista[i]),max(c,lista[i])))
     return pairs
-
