de6df8117d79f1928debc0065d069c50b5c2ae89
Alvarolpe <158449001+Alvarolpe@users.noreply.github.com>
2024-09-26 18:33:15 +0200

    versión final

--- #trabajo_algoritmos_1.py
+++ #trabajo_algoritmos_1.py
@@ -1,8 +1,12 @@
 #trabajo algoritmos 1
+# Álvaro López Pérez: alvaro.lopez.perez@udc.es
+# Luca Grygar Casas: luca.grygarc@udc.es
+
 import time
 import random
 import algoritmos1
 import pandas as pd
+import numpy as np
 
 # generar una lista de tamaño n
 def generarL(n):
@@ -16,13 +20,15 @@ def generarL(n):
     return (nums, target)
 
 # medir el tiempo de ejecución
-def medir_time(algoritmo, n):
+def medir_time_100(algoritmo, n):
+    A = False
     t1 = time.time_ns()
     x = generarL(n)
     algoritmo(x[0], x[1])
     t2 = time.time_ns()
     t = t2-t1
     if t < 500:
+        A = True
         i = 0
         k = 100
         t1 = time.time_ns()
@@ -41,36 +47,72 @@ def medir_time(algoritmo, n):
 
         t = (T1-T2)/k
     
-    return t    
+    return (t, A)
+
+def medir_time_1000(algoritmo, n):
+    A = False
+    t1 = time.time_ns()
+    x = generarL(n)
+    algoritmo(x[0], x[1])
+    t2 = time.time_ns()
+    t = t2-t1
+    if t < 500:
+        A = True
+        i = 0
+        k = 1000
+        t1 = time.time_ns()
+        while i != k:
+            x = generarL(n)
+            algoritmo(x[0], x[1])
+            i += 1
+        t2 = time.time_ns()
+        T1 = t2 - t1
+        t1 = time.time_ns()
+        while i != k:
+            x = generarL(x)
+            i += 1
+        t2 = time.time_ns()
+        T2 = t2-t1
+
+        t = (T1-T2)/k
+    
+    return (t, A)
 
 def main():
     i = 0
-    p1 = []
-    p2 =[]
+    f1 = []
+    f2 =[]
+    a1 = []
+    a2 = []
     values = []
     n = 100
     while i != 7:
         values.append(n)
-        p1.append(medir_time(algoritmos1.searchpairswithlists, n))
-        p2.append(medir_time(algoritmos1.searchpairswithsets, n))
+        m1 = medir_time_100(algoritmos1.searchpairswithlists, n)
+        m2 = medir_time_1000(algoritmos1.searchpairswithsets, n)
+        f1.append(m1[0])
+        a1.append(m2[1])
+        f2.append(m2[0])
+        a2.append(m2[1])
         i += 1
         n = 2*n
-    datos_finales_p1 = pd.DataFrame({"n": values, "Time(ns)": p1})
-    datos_finales_p2 = pd.DataFrame({"n": values, "Time(ns)": p2})
+    datos_finales_f1 = pd.DataFrame({"n": values, "Avarage": a1, "Time(ns)": f1})
+    datos_finales_f2 = pd.DataFrame({"n": values, "Avarage": a2, "Time(ns)": f2})
 
-     a1 = (datos_finales_p1["Time(ns)"]/datos_finales_p1["n"])
-    datos_finales_p1["O(n*1.5)"] = a1**1.5
-    datos_finales_p1["O(n*2)"] = a1**2
-    datos_finales_p1["O(n*2.2)"] = a1**2.2
+    n1 = datos_finales_f1["n"]
+    t1 = datos_finales_f1["Time(ns)"]
+    datos_finales_f1["t(n)/O(nlog2(n))"] = t1/(n1*np.log2(n1))
+    datos_finales_f1["t(n)/O(n^2)"] = t1/(n1**2)
+    datos_finales_f1["t(n)/O(n^2.3)"] = t1/(n1**2.3)
 
-    a2 = (datos_finales_p2["Time(ns)"]/datos_finales_p2["n"])
-    datos_finales_p2["O(log2(n))"] = np.log2(a2)
-    datos_finales_p2["O(n)"] = a2
-    datos_finales_p2["O(nlog2(n))"] = a2*np.log2(a2)
-        
-    print("\tMetodo de Listas:\n",datos_finales_p1,"\n\n")
-    print("\tMetodo de Conjuntos:\n", datos_finales_p2)
+    n2 = datos_finales_f2["n"]
+    t2 = datos_finales_f2["Time(ns)"]
+    datos_finales_f2["t(n)/O(log2(n))"] = t2/np.log2(n2)
+    datos_finales_f2["t(n)/O(n)"] = t2/n2
+    datos_finales_f2["t(n)/O(n^1.2)"] = t2/ (n2**1.2)
 
+    print("\tMetodo de Listas:\n",datos_finales_f1, "\n\n")
+    print("\tMetodo de Conjuntos:\n", datos_finales_f2)
 
 if __name__ == "__main__":
     main()
